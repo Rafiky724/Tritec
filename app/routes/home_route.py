98531596ef7fad82227e6a7b33e47b650c3b523c
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2024-09-10 17:02:29 -0500

    Front v3
    Completed first interface

--- app/routes/home_route.py
+++ app/routes/home_route.py
@@ -4,5 +4,25 @@ home_bp = Blueprint('home_bp', __name__)
 
 @home_bp.route('/')
 def home():
-    return render_template('index.html', title='FizzBuzz', description='El problema de FizzBuzz consiste en reemplazar los números de una lista con "Fizz" si son múltiplos de 3, "Buzz" si son múltiplos de 5, y "FizzBuzz" si son múltiplos de ambos.', image_url=url_for('static', filename='img/fizzbuzz-logo.png'), stars=5 )
+    cards = [
+        {
+            'title': 'FizzBuzz',
+            'description': 'El problema de FizzBuzz consiste en reemplazar los números de una lista con "Fizz" si son múltiplos de 3, "Buzz" si son múltiplos de 5, y "FizzBuzz" si son múltiplos de ambos.',
+            'image_url': url_for('static', filename='img/fizzbuzz-logo.png'),
+            'stars': 5
+        },
+        {
+            'title': 'BubleSort',
+            'description': 'El algoritmo de Bubble Sort ordena una lista comparando y, si es necesario, intercambiando pares de elementos adyacentes. Repite el proceso hasta que toda la lista esté ordenada.',
+            'image_url': url_for('static', filename='img/buble-logo.png'),
+            'stars': 3
+        },
+        {
+            'title': 'BinarySearch',
+            'description': 'El algoritmo de Binary Search busca un elemento en una lista ordenada dividiendo repetidamente el rango de búsqueda a la mitad, comparando el elemento buscado con el valor en el punto medio, y ajustando el rango según corresponda.',
+            'image_url': url_for('static', filename='img/binary-logo.png'),
+            'stars': 4
+        },
+    ]
 
+    return render_template('index.html', cards=cards)
--- app/routes/problem_route.py
+++ app/routes/problem_route.py
@@ -2,6 +2,15 @@ from flask import Blueprint, render_template
 
 problem_bp = Blueprint('problem_bp', __name__)
 
-@problem_bp.route('/problemas')
+@problem_bp.route('/problems')
 def problems():
-    return render_template('problems.html')
+    problems = [
+        {
+            'title': 'FizzBuzz',
+            'star': 1,
+            'test': 2,
+            'description': 'oli'
+         },
+    ]
+    
+    return render_template('problems.html', problems=problems)
