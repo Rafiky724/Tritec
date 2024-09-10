212f769da0be18d7d4dd55e18c83cdab1de08132
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2024-09-10 16:20:24 -0500

    Front v2
    card reuse process

--- app/routes/home_route.py
+++ app/routes/home_route.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 
 home_bp = Blueprint('home_bp', __name__)
 
 @home_bp.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title='FizzBuzz', description='El problema de FizzBuzz consiste en reemplazar los números de una lista con "Fizz" si son múltiplos de 3, "Buzz" si son múltiplos de 5, y "FizzBuzz" si son múltiplos de ambos.', image_url=url_for('static', filename='img/fizzbuzz-logo.png'), stars=5 )
 
