debd3d48bcaf595e2409efdbf0656442b6cb6b35
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2024-09-14 16:06:27 -0500

    Update Routes
    Updated routes to exist in a single file called routesData.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from app.routes.home_route import home_bp
-from app.routes.problem_route import problem_bp
+from app.routes.routesData import bp
 from config import Config
 
 def create_app():
@@ -9,7 +8,6 @@ def create_app():
     app.config.from_object(Config)
     
     # Registrar Blueprints
-    app.register_blueprint(home_bp, url_prefix='/')
-    app.register_blueprint(problem_bp, url_prefix='/')
+    app.register_blueprint(bp)
     
     return app
--- app/routes/routesData.py
+++ app/routes/routesData.py
@@ -1,12 +1,37 @@
 from flask import Blueprint, render_template, request, url_for
 
-problem_bp = Blueprint('problem_bp', __name__)
+bp = Blueprint('bp', __name__)
 
-@problem_bp.route('/problems')
+@bp.route('/')
+def home():
+    cards = [
+        {},
+        {
+            'title': 'FizzBuzz',
+            'description': 'El problema de FizzBuzz consiste en reemplazar los números de una lista con "Fizz" si son múltiplos de 3, "Buzz" si son múltiplos de 5, y "FizzBuzz" si son múltiplos de ambos.',
+            'image_url': url_for('static', filename='img/fizzbuzz-logo.png'),
+            'stars': 5
+        },
+        {
+            'title': 'BubleSort',
+            'description': 'El algoritmo de Bubble Sort ordena una lista comparando y, si es necesario, intercambiando pares de elementos adyacentes. Repite el proceso hasta que toda la lista esté ordenada.',
+            'image_url': url_for('static', filename='img/buble-logo.png'),
+            'stars': 3
+        },
+        {
+            'title': 'BinarySearch',
+            'description': 'El algoritmo de Binary Search busca un elemento en una lista ordenada dividiendo repetidamente el rango de búsqueda a la mitad, comparando el elemento buscado con el valor en el punto medio, y ajustando el rango según corresponda.',
+            'image_url': url_for('static', filename='img/binary-logo.png'),
+            'stars': 4
+        },
+    ]
+
+    return render_template('index.html', cards=enumerate(cards))
+
+@bp.route('/problems')
 def problems():
-    
     value = request.args.get('value', default=None, type=int)
-    
+
     problems = [
         {},
         {
@@ -43,11 +68,11 @@ def problems():
             },
         },
     ]
-    
+
     for problem in problems:
         if 'problem' in problem:
             problem['problem'] = problem['problem'].replace('\n', '<br>')
-    
+
     problem = problems[value] if value is not None and 0 <= value < len(problems) else None
-    
+
     return render_template('problems.html', problem=problem)
