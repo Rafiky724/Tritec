d877c66c326c8159da7c1cfd8a6fb1a23bd56063
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2024-10-08 21:58:33 -0500

    Update Interface
    Update color interface for problems code

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,7 +1,7 @@
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
   mode: "python",
   lineNumbers: true,
-  theme: "default",
+  theme: "dracula",
   tabSize: 4,
   indentUnit: 4,
   lineWrapping: true,
