4d5c0d108fede3a8d73892fd531d7ee528a746fb
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2024-10-09 18:26:16 -0500

    Update problems
    Include this MoneyToEnglish and image change

--- app/routes/routesData.py
+++ app/routes/routesData.py
@@ -28,37 +28,37 @@ def home():
             'title': 'FizzBuzz',
             'description': 'El problema de FizzBuzz consiste en reemplazar los números de una lista con "Fizz" si son múltiplos de 3, "Buzz" si son múltiplos de 5, y "FizzBuzz" si son múltiplos de ambos.',
             'image_url': url_for('static', filename='img/fizzbuzz-logo.png'),
-            'stars': 5
+            'stars': 3
         },
         {
             'title': 'Palindrome',
             'description': 'El algoritmo para verificar si una palabra es un palíndromo normaliza la entrada eliminando espacios y convirtiendo todas las letras a minúsculas. Luego, compara la palabra resultante con su versión invertida. Si ambas versiones son iguales, la palabra es un palíndromo; de lo contrario, no lo es.',
-            'image_url': url_for('static', filename='img/buble-logo.png'),
-            'stars': 3
+            'image_url': url_for('static', filename='img/palindrome-logo.png'),
+            'stars': 2
         },
         {
             'title': 'BinarySearch',
             'description': 'El algoritmo de Binary Search busca un elemento en una lista ordenada dividiendo repetidamente el rango de búsqueda a la mitad, comparando el elemento buscado con el valor en el punto medio, y ajustando el rango según corresponda.',
             'image_url': url_for('static', filename='img/binary-logo.png'),
-            'stars': 4
+            'stars': 2
         },
         {
             'title': 'IntegerToRoman',
             'description': 'El algoritmo de Integer to Roman convierte un número entero a su representación en números romanos utilizando un enfoque de resta. Se utilizan listas de valores y símbolos romanos para formar la representación correcta a partir de la entrada numérica.',
-            'image_url': url_for('static', filename='img/binary-logo.png'),
-            'stars': 5
+            'image_url': url_for('static', filename='img/integerToRoman-logo.png'),
+            'stars': 3
         },
         {
             'title': 'RomanToInteger',
-            'description': 'AA',
-            'image_url': url_for('static', filename='img/binary-logo.png'),
-            'stars': 4
+            'description': 'El algoritmo RomanToInteger convierte números romanos a enteros, analizando los símbolos y aplicando reglas de suma y resta según su posición. Se procesa la cadena de derecha a izquierda para calcular el valor total.',
+            'image_url': url_for('static', filename='img/romanToInteger-logo.png'),
+            'stars': 3
         },
         {
             'title': 'MoneyToEnglish',
-            'description': 'AA',
-            'image_url': url_for('static', filename='img/binary-logo.png'),
-            'stars': 1
+            'description': 'El algoritmo MoneyToEnglish convierte una cantidad monetaria en su representación textual en inglés. Descompone el número en partes enteras y decimales, convirtiendo cada una en palabras para generar una salida clara y correcta.',
+            'image_url': url_for('static', filename='img/moneyToEnglish-logo.png'),
+            'stars': 4
         },
     ]
 
@@ -74,7 +74,7 @@ def problems():
             'title': 'FizzBuzz',
             'problem': 'El objetivo del ejercicio es verificar si un numero dado cumple las condiciones: \n\n Divisibilidad por 3: Si el numero es divisible por 3, devolverás "Fizz". \n\n Divisibilidad por ambos: Si el numero es divisible por 5, devolverás "Buzz". \n\n Divisibilidad por 3: Si el numero es divisible por ambos, devolveras "FizzBuzz". \n\n No es divisible ni por 3 ni por 5: Si el número no es divisible ni por 3 ni por 5, devolverás el número en forma de cadena. \n\n Entrada: n: int : Un número entero (para python) o un numero long (para c#). \n\n Salidas: str: devuelve "Fizz", "Buzz", "FizzBuzz", o el número en forma de cadena.',
             'image_url': url_for('static', filename='img/fizzbuzz-logo.png'),
-            'stars': 5,
+            'stars': 3,
             "languages": {
                 'python': {'code': 'def fizzbuzz(n):\n\n    #ESCRIBE TU CÓDIGO AQUÍ'},
                 "c#": {
@@ -85,8 +85,8 @@ def problems():
         {
             'title': 'Palindrome',
             'problem': 'El objetivo es verificar si una palabra es palíndromo. Un palíndromo es una palabra que se lee igual de izquierda a derecha y de derecha a izquierda, ignorando los espacios. \n\n Entrada: word: str \n\n Salidas: bool : True si la palabra es palindromo, False en caso contrario.',
-            'image_url': url_for('static', filename='img/buble-logo.png'),
-            'stars': 3,
+            'image_url': url_for('static', filename='img/palindrome-logo.png'),
+            'stars': 2,
             'languages': {
                 'python': {'code': 'def is_palindrome(word):\n\n    #ESCRIBE TU CÓDIGO AQUÍ'},
                 'c#': {'code': 'using TritecAPI.Interfaces;\n\nnamespace TritecAPI.problem_solver\n{\n    public class Palindrome\n    {\n        public bool _palindrome(string word)\n        {\n            //ESCRIBE TU CÓDIGO AQUÍ\n        }\n    }\n}'}
@@ -96,7 +96,7 @@ def problems():
             'title': 'BinarySearch',
             'problem': 'Realiza una busqueda binario en un arreglo ordenado para encontrar la posicion de un valor objetivo. \n\n Entrada: arr (Un arreglo ordenado de elementos), target(El valor que se desea encontrar).\n\n Salida: int: El indice del target en el arreglo si se encuentra. En caso contrario, se devuelve -1). ',
             'image_url': url_for('static', filename='img/binary-logo.png'),
-            'stars': 4,
+            'stars': 2,
             'languages': {
                 'python': {'code': 'def binary_search(arr, target):\n\n    #ESCRIBE TU CÓDIGO AQUÍ'},
                 'c#': {'code': 'using TritecAPI.Interfaces;\n\nnamespace TritecAPI.problem_solver\n{\n    public class BinarySearch\n    {\n        public int _binarySearch(int[] array, int target)\n        {\n            //ESCRIBE TU CÓDIGO AQUÍ\n        }\n    }\n}'}
@@ -104,9 +104,9 @@ def problems():
         },
         {
             'title': 'IntegerToRoman',
-            'problem': 'Dada un entero, convierte el número en su representación en números romanos. \n\n Entrada: num (Un entero entre 1 y 3999).\n\n Salida: string: La representación en números romanos del entero proporcionado.',
-            'image_url': url_for('static', filename='img/binary-logo.png'),
-            'stars': 5,
+            'problem': 'Dada un entero, convierte el número en su representación en números romanos. Los números romanos utilizan combinaciones de letras como I, V, X, L, C, D y M para representar valores. \n\n Entrada: num (Un entero entre 1 y 3999). \n\n Salida: string: La representación en números romanos del entero proporcionado.',
+            'image_url': url_for('static', filename='img/integerToRoman-logo.png'),
+            'stars': 3,
             'languages': {
                 'python': {'code': 'def integer_to_roman(num):\n\n    #ESCRIBE TU CÓDIGO AQUÍ'},
                 'c#': {'code': 'x'}
@@ -114,9 +114,9 @@ def problems():
         },
         {
             'title': 'RomanToInteger',
-            'problem': 'aa',
-            'image_url': url_for('static', filename='img/binary-logo.png'),
-            'stars': 4,
+            'problem': 'Dada una cadena que representa un número romano, convierte esa cadena en un entero. Los números romanos están compuestos por los siguientes caracteres: I, V, X, L, C, D y M, cada uno de los cuales tiene un valor asociado. Para algunos números romanos, se utilizan combinaciones específicas de estos caracteres para representar valores únicos. \n\n Entrada: s: str (Una cadena que representa un número romano). \n\n Salida: int: La representación entera del número romano proporcionado.',
+            'image_url': url_for('static', filename='img/romanToInteger-logo.png'),
+            'stars': 3,
             'languages': {
                 'python': {'code': 'def roman_to_integer(num):\n\n    #ESCRIBE TU CÓDIGO AQUÍ'},
                 'c#': {'code': 'x'}
@@ -124,9 +124,9 @@ def problems():
         },
         {
             'title': 'MoneyToEnglish',
-            'problem': 'aa',
-            'image_url': url_for('static', filename='img/binary-logo.png'),
-            'stars': 1,
+            'problem': 'Dada una cantidad de dinero en formato numérico, convierte esa cantidad en su representación en inglés. Por ejemplo, 123.45 debería convertirse en "One Hundred Twenty-Three Dollars and Forty-Five Cents". \n\n Entrada: num: float (Un número que representa la cantidad de dinero). \n\n Salida: str: La representación en inglés de la cantidad de dinero proporcionada.',
+            'image_url': url_for('static', filename='img/moneyToEnglish-logo.png'),
+            'stars': 4,
             'languages': {
                 'python': {'code': 'def money_to_english(num):\n\n    #ESCRIBE TU CÓDIGO AQUÍ'},
                 'c#': {'code': 'x'}
